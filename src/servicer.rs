dfed74e9f2adf964607c351e1e47a64e4f28092c
akakou <akama@keio.jp>
2020-11-04 21:46:25 +0900

    [add] Verify servicer

--- src/servicer.rs
+++ src/servicer.rs
@@ -1,10 +1,54 @@
-use crate::utils::{generate_public_key, hash_sha256};
-use k256::{EncodedPoint, Scalar};
+use crate::utils::{generate_public_key, hash_sha256, biguint_to_scalar, scalar_to_biguint};
+use k256::{EncodedPoint, Scalar, ProjectivePoint, NonZeroScalar};
 use num_bigint::BigUint;
+// use elliptic_curve::{ FromDigest, ff::PrimeField };
+use sha2::{Digest, Sha256};
 use rand::{CryptoRng, RngCore};
 
 pub struct Servicer {
     pub id: u8,
     pub R: EncodedPoint,
-    pub S: BigUint,
+    pub S: NonZeroScalar,
+    pub PK: Option<EncodedPoint>,
+    pub PKas: EncodedPoint
+}
+
+impl Servicer {
+    pub fn is_valid(&self) -> bool {
+        // left
+        let left = generate_public_key(&self.S);
+        let left = left.decode::<ProjectivePoint>().unwrap();
+        
+        // right
+        let mut hash = self.R.to_bytes().to_vec();
+
+        hash.push(00 as u8);
+        hash.push(self.id as u8);
+        let hash = hash_sha256(&hash);
+        let hash = biguint_to_scalar(&hash);
+        let hash = NonZeroScalar::new(hash).unwrap();
+
+        let PKas = self.PKas.decode::<ProjectivePoint>().unwrap();
+        let R = self.R.decode::<ProjectivePoint>().unwrap();
+        let right = R + PKas * &*hash;
+
+        println!("left. {:?}", left);
+        println!("right. {:?}", right);
+
+        //  PKWS = SWS·P = RWS + H1(IDWS‖RWS)·PK
+        left == right
+    }
+}
+
+#[test]
+fn test_verify_servicer() {
+    use crate::authority::Authority;
+
+    let mut rng = rand::thread_rng();
+
+    let authority = Authority::random(rng);
+    let servicer = authority.register_servicer(10, &mut rng);
+
+    let result = servicer.is_valid();
+    assert!(result);
 }
