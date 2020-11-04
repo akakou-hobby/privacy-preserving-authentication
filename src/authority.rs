6d72ae586689ae81946296672855b7e6ed159862
akakou <akama@keio.jp>
2020-11-04 21:45:38 +0900

    [fix] Calculation miss

--- src/authority.rs
+++ src/authority.rs
@@ -1,65 +1,68 @@
 use crate::servicer::Servicer;
-use crate::utils::{generate_public_key, hash_sha256};
+use crate::utils::{generate_public_key, hash_sha256, scalar_to_biguint, biguint_to_scalar};
 
 use k256::{EncodedPoint, NonZeroScalar, ProjectivePoint};
 use num_bigint::BigUint;
 use rand::{CryptoRng, RngCore};
 
+#[derive(Clone)]
 pub struct Authority {
     pub s: NonZeroScalar,
-    pub PK: EncodedPoint,
+    pub PK: EncodedPoint
 }
 
 impl Authority {
-    pub fn random(rng: impl CryptoRng + RngCore) -> Self {
+    pub fn random(rng: impl RngCore + CryptoRng) -> Self {
         let s = NonZeroScalar::random(rng);
         let PK = generate_public_key(&s);
 
-        Self { s: s, PK: PK }
+        Self { s: s, PK: PK}
     }
 
-    pub fn register_servicer(&self, id: u8, rng: impl CryptoRng + RngCore) -> Servicer {
-        let register = ServicerRegister::random(self.s, id, rng);
-        register.register()
+    pub fn register_servicer(&self, id: u8, rng: &mut (impl CryptoRng + RngCore)) -> Servicer {
+        let register = ServicerRegister::random(self.clone(), id, rng);
+        register.register(rng)
     }
 }
 
 pub struct ServicerRegister {
     id: u8,
     r: NonZeroScalar,
-    s: NonZeroScalar,
+    authority: Authority
 }
 
 impl ServicerRegister {
-    pub fn random(s: NonZeroScalar, id: u8, rng: impl CryptoRng + RngCore) -> Self {
+    pub fn random(authority: Authority, id: u8, rng: &mut (impl CryptoRng + RngCore)) -> Self {
         Self {
             id: id,
-            s: s,
-            r: NonZeroScalar::random(rng),
+            authority: authority,
+            r: NonZeroScalar::random(rng)
         }
     }
 
-    pub fn register(&self) -> Servicer {
-        let R = generate_public_key(&self.s);
+    pub fn register(&self, rng: &mut (impl CryptoRng + RngCore)) -> Servicer {
+        let r = NonZeroScalar::random(rng);
+        let R = generate_public_key(&r);
 
         let mut hash = R.to_bytes().to_vec();
         hash.push(00 as u8);
         hash.push(self.id as u8);
 
         let hash = hash_sha256(&hash);
+        let hash = biguint_to_scalar(&hash);
 
-        let r = self.r.invert().unwrap().truncate_to_u32();
-        let r = BigUint::from(r);
+        let r = r.as_ref();
+        let s = self.authority.s.as_ref();
 
-        let S = self.s.invert().unwrap().truncate_to_u32();
-        let S = BigUint::from(S);
-
-        let S = r + hash * S;
+        let S = r + hash * s;
+        let S = NonZeroScalar::new(S).unwrap();
 
         Servicer {
             id: self.id,
             R: R,
             S: S,
+            PK: None,
+            PKas: self.authority.PK
         }
     }
 }
@@ -72,8 +75,8 @@ fn test_generate_authority() {
 
 #[test]
 fn test_register_servicer() {
-    let rng = rand::thread_rng();
+    let mut rng = rand::thread_rng();
 
     let authority = Authority::random(rng);
-    authority.register_servicer(10, rng);
+    authority.register_servicer(10, &mut rng);
 }
